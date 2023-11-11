37205090aed346f390a84f30bfa739cba144e58d
AstridPerezGaray <astridperezg3@gmail.com>
2023-11-11 08:39:14 -0600

    Se arreglo por los cambios en la BD

--- ProcedimientosAlmacenados.sql
+++ ProcedimientosAlmacenados.sql
@@ -1,26 +1,46 @@
 USE Agroservicio;
 GO
 
--- Procedimiento almacenado de inserción para agregar un nuevo empleado
 CREATE PROCEDURE AgregarNuevoEmpleado
 (
     @NombresEmpleado VARCHAR(60),
     @ApellidosEmpleado VARCHAR(60),
+    @FechaNac DATE,
     @Telefono VARCHAR(10),
     @Correo VARCHAR(100),
-    @DUI VARCHAR(60),
-    @ISSS VARCHAR(15),
-    @ID_Direccion INT
+    @DUI VARCHAR(10),
+    @ISSS INT,
+    @ID_Direccion INT,
+    @ID_TipoEmpleado INT
 )
 AS
 BEGIN
-    INSERT INTO Empleados (NombresEmpleado, ApellidosEmpleado, Telefono, Correo, DUI, ISSS, ID_Direccion)
-    VALUES (@NombresEmpleado, @ApellidosEmpleado, @Telefono, @Correo, @DUI, @ISSS, @ID_Direccion);
+    INSERT INTO Empleados (NombresEmpleado, ApellidosEmpleado, FechaNac, Telefono, Correo, DUI, ISSS, ID_Direccion, ID_TipoEmpleado)
+    VALUES (@NombresEmpleado, @ApellidosEmpleado, @FechaNac, @Telefono, @Correo, @DUI, @ISSS, @ID_Direccion, @ID_TipoEmpleado);
 END;
 
-
 -- EXEC del procedimiento de inserción
-EXEC AgregarNuevoEmpleado 'Juan', 'Pérez', '1234567890', 'juan@example.com', '1234567890', '12345', 1; -- Cambia los valores según tus necesidades
+EXEC AgregarNuevoEmpleado 
+    @NombresEmpleado = 'Juan',
+    @ApellidosEmpleado = 'Perez',
+    @FechaNac = '1990-05-15',
+    @Telefono = '777555444',
+    @Correo = 'juan.perez@example.com',
+    @DUI = '123456789',
+    @ISSS = 123456789,
+    @ID_Direccion = 1,
+    @ID_TipoEmpleado = 1; 
+
+EXEC AgregarNuevoEmpleado 
+    @NombresEmpleado = 'María',
+    @ApellidosEmpleado = 'García',
+    @FechaNac = '1985-09-20',
+    @Telefono = '555333222',
+    @Correo = 'maria.garcia@example.com',
+    @DUI = '987654321',
+    @ISSS = 987654321,
+    @ID_Direccion = 1, 
+    @ID_TipoEmpleado = 1; 
 
 USE Agroservicio;
 GO
@@ -33,8 +53,8 @@ BEGIN
     FROM Empleados E
     JOIN Direcciones Direccion ON E.ID_Direccion = Direccion.ID_Direccion
     JOIN Distritos Distrito ON Direccion.ID_Distrito = Distrito.ID_Distrito
-    JOIN Municipio Municipio ON Distrito.ID_Municipio = Municipio.ID_Municipio
-    JOIN Departamento D ON Municipio.ID_Departamento = D.ID_Departamento;
+    JOIN Municipios Municipio ON Distrito.ID_Municipio = Municipio.ID_Municipio
+    JOIN Departamentos D ON Municipio.ID_Departamento = D.ID_Departamento;
 END;
 
 -- EXEC del procedimiento de consulta
